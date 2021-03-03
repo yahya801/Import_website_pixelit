08b4fbb62dbd358ff87d0a28143a4fcaa9028213
Rasheeda Abbas <rasheedaabbas@gmail.com>
2021-03-03 23:53:32 +0500

    form validator

--- form_check.php
+++ form_check.php
@@ -31,11 +31,75 @@ if ($conn->connect_error) {
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "BrandId: " . $row["brandID"]. " - Brandname: " . $row["brandname"].  "<br>";
+      echo "BrandId: " . $row["brandID"]. " - URL: " . $row["url"].  "<br>";
+      $parse = parse_url($url, PHP_URL_HOST);
+      $parse_path =  parse_url($url, PHP_URL_PATH);
+      $parseU1 = parse_url($row["url"], PHP_URL_HOST);
+      $parse_final = "";
+      echo $parse. "<br>";
+      echo $parse_path. "<br>";
+      if ($row['urlmobile'] != NULL){
+        $parseM = parse_url($row["urlmobile"], PHP_URL_HOST);
+        if($parseM == $parse){
+          $parse_final = $parseM;
+        } 
+        else if ($parseU1 == $parse)
+        {
+          $parse_final = $parseU1;
+        } else {
+          if ($parse == $parseU2){
+          $parseU2 = parse_url($row["url2"], PHP_URL_HOST);
+          $parse_final = $parseU2;}
+          else {
+            echo "not right";
+          }
+        }
+      }
+      else if ($row['url2'] != NULL) 
+      {
+        $parseU2 = parse_url($row["url2"], PHP_URL_HOST);
+        if ($parseU2 == $parse) {
+          $parse_final = $parseU2;
+        }
+        else {
+          if ($parse == $parseU1){
+          $parse_final = $parseU1;}
+          else {
+            echo "not right";
+          }
+        }
+      } 
+      else 
+      {
+        if ($parse == $parseU1){
+        $parse_final = $parseU1;
+        }
+        else {
+          echo "not right";
+        }
+      }
     }
   } else {
     echo "0 results";
   }
+  echo $parse_final;
+  $oo = 0;
+  $pattern = array("/uk/", "/gb/", "/en_gb/", "/en-gb/", "/GB/", "/en_gbp/","/en_GB/");
+  for ($x = 0; $x < sizeof($pattern); $x++) {
+    $oo = preg_match($pattern[$x], $parse_path);
+    if ($oo == 1) {
+        $parse1 = "okay";
+        $parse2 = "okay";
+        break;
+    }    
+}
+$reg = "~.*([^\.]+)(co\.uk)$~";
+if ($oo == 0 && preg_match("~\.co\.uk~", $parse_final) == 0){
+  echo "wrong url";
+}
+else echo "correct url";
+echo $oo;
+
 
 }
 ?> 
